e6b000052c32866ff20218b30cbc6102bea5aff6
tiger0427 <capacitor0427@gmail.com>
2022-04-24 21:57:15 +0900

    Fix payload
    encodeURI, JSON.stringfy

--- functions/src/notification.ts
+++ functions/src/notification.ts
@@ -6,17 +6,18 @@ import {WordpressHook} from "./type";
 export const wordpressPublished = functions.https.onRequest(
     async (req, res) => {
       const hook = req.body as WordpressHook;
+      hook.imageUrl = encodeURI(hook.imageUrl);
       const payload: messaging.TopicMessage = {
         topic: "wordpress-publish",
-        data: {
-          title: hook.title,
-          desctiption: hook.desctiption,
-          imageUrl: hook.imageUrl,
-        },
         notification: {
           title: hook.title,
           body: hook.desctiption,
           imageUrl: hook.imageUrl,
+        },
+        data: {
+          title: JSON.stringify({title: hook.title}),
+          desctiption: JSON.stringify({description: hook.desctiption}),
+          imageUrl: JSON.stringify({imageUrl: hook.imageUrl}),
         },
         android: {
           priority: "high",
@@ -41,7 +42,11 @@ export const wordpressPublished = functions.https.onRequest(
         },
       };
       initializeApp({credential: applicationDefault()});
-      messaging().send(payload);
+      try {
+        await messaging().send(payload);
+      } catch (e) {
+        console.log(e);
+      }
       res.end();
     }
 );
