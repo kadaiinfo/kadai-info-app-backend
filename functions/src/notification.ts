171890e5835ce65089f48c3256d8a380e08495d6
tiger0427 <capacitor0427@gmail.com>
2022-04-24 19:54:16 +0900

    Add fcm payload

--- functions/src/notification.ts
+++ functions/src/notification.ts
@@ -1,10 +1,47 @@
+import {initializeApp, messaging} from "firebase-admin";
+import {applicationDefault} from "firebase-admin/app";
 import * as functions from "firebase-functions";
 import {WordpressHook} from "./type";
 
 export const wordpressPublished = functions.https.onRequest(
     async (req, res) => {
       const hook = req.body as WordpressHook;
-      console.log(hook);
+      const payload: messaging.TopicMessage = {
+        topic: "wordpress-publish",
+        data: {
+          title: hook.title,
+          desctiption: hook.desctiption,
+          imageUrl: hook.imageUrl,
+        },
+        notification: {
+          title: hook.title,
+          body: hook.desctiption,
+          imageUrl: hook.imageUrl,
+        },
+        android: {
+          priority: "high",
+          notification: {
+            priority: "high",
+            visibility: "public",
+          },
+        },
+        apns: {
+          headers: {
+            "apns-push-type": "alert",
+          },
+          payload: {
+            aps: {
+              mutableContent: true,
+              contentAvailable: true,
+            },
+          },
+          fcmOptions: {
+            imageUrl: hook.imageUrl,
+          },
+        },
+      };
+      initializeApp({credential: applicationDefault()});
+      messaging().send(payload);
       res.end();
     }
 );
